733be57e451762d64a3fdc2da80626bf0dce34f7
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-01-31 14:59:56 +0100

    add test for _create_es_mapping

---
+++ tests/elasticsearch/test_create_es_mapping.py
@@ -0,0 +1,14 @@
+import json
+
+from karp.elasticsearch.index import _create_es_mapping
+
+
+def test_places():
+    with open('tests/data/config/places.json') as f:
+        schema = json.load(f)
+
+    mappings = _create_es_mapping(schema)
+
+    print(repr(mappings))
+    assert mappings is None
+    
