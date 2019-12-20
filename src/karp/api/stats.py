4fc711ddb5f0474732ca940198757c7e4ff64edf
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2019-12-20 08:58:29 +0100

    use context

--- src/karp/api/stats.py
+++ src/karp/api/stats.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, jsonify    # pyre-ignore
+from flask import Blueprint, jsonify  # pyre-ignore
 
-from karp import search
+# from karp import search
+from karp.context import ctx
 import karp.auth.auth as auth
 
-stats_api = Blueprint('stats_api', __name__)
+stats_api = Blueprint("stats_api", __name__)
 
 
-@stats_api.route('/<resource_id>/stats/<field>', methods=['GET'])
-@auth.auth.authorization('READ')
+@stats_api.route("/<resource_id>/stats/<field>", methods=["GET"])
+@auth.auth.authorization("READ")
 def get_field_values(resource_id, field):
-    return jsonify(search.search.statistics(resource_id, field))
+    return jsonify(ctx.search.statistics(resource_id, field))
