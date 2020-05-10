ef330e939eced254edc574b4076e2345216c90e3
Kristoffer Andersson <kod.kristoff@gmail.com>
2020-05-10 10:47:50 +0200

    add sql_legacy stub

---
+++ karp/infrastructure/sql/legacy_entry_repository.py
@@ -0,0 +1,9 @@
+from typing import Dict
+
+from karp.domain.model.entry import EntryRepository
+
+
+class SqlLegacyEntryRepository(EntryRepository, repository_type="sql_legacy_v1"):
+    @classmethod
+    def from_dict(cls, settings: Dict):
+        pass
---
+++ tests/unit_tests/infrastructure/sql/test_sql_legacy_entry_repository.py
@@ -0,0 +1,20 @@
+from karp.domain.model.entry import (
+    create_entry,
+    EntryRepository,
+)
+
+from karp.infrastructure.sql.legacy_entry_repository import SqlLegacyEntryRepository
+
+
+def test_create_sql_legacy_entry_repository():
+    entry_repo, repo_type = EntryRepository.create(
+        "sql_legacy_v1",
+        {
+            "db_uri": "sqlite:///",
+            "table_name": "karpentry",
+            "lexicon_id": "test_legacy_lexicon"
+        }
+    )
+
+    assert repo_type == "sql_legacy_v1"
+    assert isinstance(entry_repo, SqlLegacyEntryRepository)
