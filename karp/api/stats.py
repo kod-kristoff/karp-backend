3695ae203cf807a836bf70ef947da39232138adb
Kristoffer Andersson <6058164+kod-kristoff@users.noreply.github.com>
2020-08-24 13:30:15 +0200

    refactor: use application.context

--- karp/api/stats.py
+++ karp/api/stats.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify  # pyre-ignore
 
-from karp import search
+from karp.application import context
 import karp.auth.auth as auth
 
 stats_api = Blueprint("stats_api", __name__)
@@ -9,4 +9,4 @@ stats_api = Blueprint("stats_api", __name__)
 @stats_api.route("/<resource_id>/stats/<field>", methods=["GET"])
 @auth.auth.authorization("READ")
 def get_field_values(resource_id: str, field: str):
-    return jsonify(search.statistics(resource_id, field))
+    return jsonify(context.search_index.statistics(resource_id, field))
