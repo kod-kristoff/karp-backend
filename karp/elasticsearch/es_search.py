2c95e723d8bf8130dede19a29345cfcb97e700b8
Kristoffer Andersson <kristoffer.andersson@gu.se>
2021-04-26 14:32:07 +0200

    fix: fetch more than 10000

--- karp/elasticsearch/es_search.py
+++ karp/elasticsearch/es_search.py
@@ -1,11 +1,12 @@
 import logging
 import re
 import json
+import itertools
+from typing import Any, Dict, List, Optional, Set, Tuple, Union
 
 import elasticsearch
 import elasticsearch_dsl as es_dsl  # pyre-ignore
-
-from typing import Any, Dict, List, Optional, Set, Tuple, Union
+import pydantic
 
 from karp.query_dsl import basic_ast as ast, op, is_a
 
@@ -17,6 +18,13 @@ from .es_observer import OnPublish
 
 
 logger = logging.getLogger("karp")
+
+
+class SearchSettings(pydantic.BaseModel):
+    scan_limit: int = 10000
+
+
+search_settings = SearchSettings()
 
 
 class EsQuery(search.Query):
@@ -660,7 +668,32 @@ class EsSearch(search.SearchInterface):
 
         result = {
             "total": response.hits.total,
-            "hits": [format_entry(entry) for entry in response],
+            "hits": [format_entry(entry) for entry in response.hits],
+        }
+        return result
+
+    def _format_result_dict(self, resource_ids, response: Dict) -> Dict:
+        def format_entry(entry):
+            dict_entry = entry["_source"]
+            version = dict_entry.pop("_entry_version", None)
+            last_modified_by = dict_entry.pop("_last_modified_by", None)
+            last_modified = dict_entry.pop("_last_modified", None)
+            return {
+                "id": entry["_id"],
+                "version": version,
+                "last_modified": last_modified,
+                "last_modified_by": last_modified_by,
+                "resource": next(
+                    resource
+                    for resource in resource_ids
+                    if entry["_index"].startswith(resource)
+                ),
+                "entry": dict_entry,
+            }
+
+        result = {
+            "total": response["hits"]["total"],
+            "hits": [format_entry(entry) for entry in response["hits"]["hits"]],
         }
         return result
 
@@ -702,7 +735,7 @@ class EsSearch(search.SearchInterface):
             if query.query is not None:
                 s = s.query(query.query)
 
-            s = s[query.from_ : query.from_ + query.size]
+            # s = s[query.from_ : query.from_ + query.size]
 
             if query.lexicon_stats:
                 s.aggs.bucket(
@@ -716,20 +749,66 @@ class EsSearch(search.SearchInterface):
                     sort_fields.extend(self.translate_sort_fields([resource], sort))
                 s = s.sort(*sort_fields)
             logger.debug("s = {}".format(s.to_dict()))
-            response = s.execute()
+            response = self.execute_query(s, from_=query.from_, size=query.size)
 
             # TODO format response in a better way, because the whole response takes up too much space in the logs
             # logger.debug('response = {}'.format(response.to_dict()))
 
-            result = self._format_result(query.resources, response)
+            print(f"{response=}")
+            result = self._format_result_dict(query.resources, response)
             if query.lexicon_stats:
                 result["distribution"] = {}
-                for bucket in response.aggregations.distribution.buckets:
+                for bucket in response["aggregations"]["distribution"]["buckets"]:
                     key = bucket["key"]
                     value = bucket["doc_count"]
                     result["distribution"][key.rsplit("_", 1)[0]] = value
 
             return result
+
+    def execute_query(
+        self, es_search: es_dsl.Search, *, from_: int = 0, size: Optional[int] = None
+    ) -> Dict:
+        if from_ is None:
+            raise ValueError("'from_' must have a value.")
+
+        response = {"hits": {"hits": [], "total": 0}}
+
+        if size is None or (from_ + size > search_settings.scan_limit):
+            # tmp_search = es_search.extra(from_=0, size=0)
+            # tmp_response = tmp_search.execute()
+            # tot_hits = tmp_response.hits.total
+            tot_hits = es_search.count()
+            response["hits"]["total"] = tot_hits
+            if size is None:
+                size = tot_hits - from_
+            if tot_hits < from_:
+                return response
+
+        if size + from_ <= search_settings.scan_limit:
+            extra_kwargs = {}
+            if from_ is not None:
+                extra_kwargs["from_"] = from_
+            if size is not None:
+                extra_kwargs["size"] = size
+            if extra_kwargs:
+                es_search = es_search.extra(**extra_kwargs)
+            return es_search.execute().to_dict()
+        else:
+            es_search = es_search.params(preserve_order=True, scroll="5m")
+            # Workaround
+            scan_iter = elasticsearch.helpers.scan(
+                es_search._using,
+                query=es_search.to_dict(),
+                index=es_search._index,
+                doc_type=es_search._get_doc_type(),
+                **es_search._params,
+            )
+            # scan_iter = es_search.scan()
+            for hit in itertools.islice(scan_iter, from_, from_ + size):
+                response["hits"]["hits"].append(hit)
+                # response["hits"]["hits"].append(hit.to_dict())
+
+            return response
 
     def translate_sort_fields(
         self, resources: List[str], sort_values: List[str]
